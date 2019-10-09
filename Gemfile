a9b822fff69a0e395e5467a8e79b86c1abdc95bb
Cody Stamps <crstamps2@gmail.com>
2019-10-08 21:44:48 -0400

    start calendly integration
    didnt realize that the v2 endpoint is oauth protected
    instead of directly token protected like the v1 endpoint.
    
    I put in a request for oauth app access, pending approval.

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'attr_encrypted'
 gem 'httparty'
+gem 'doorkeeper'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/services/feed_service.rb
+++ app/services/feed_service.rb
@@ -1,7 +1,20 @@
+require 'httparty'
+
 class FeedService
   def initialize; end
   
   def get_events
-    true
+    HTTParty.get(
+      "https://calendly.com/api/v2/users/me/events",
+      headers: headers
+    )
+  end
+
+  private
+
+  def headers
+    {
+      "X-Token": "KONMLKHMLN25PSSYXXEW6NN3O37NHVRC"
+    }
   end
 end
